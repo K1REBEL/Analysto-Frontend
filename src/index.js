7595cb8f2f88a605c31d83a4c805ee4010b4b889
hoda <hodamagdi88@gmail.com>
2024-03-14 14:56:39 +0200

    hoda

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -7,50 +6,28 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import Home from "./pages/home/Home";
 import Admin from "./pages/Admins/Admin.jsx";
 import Login from "./pages/Auth/Login";
-import Request from "./pages/Org/Request";
-import Card from "./pages/User/card.js";
-import Pass from "./pages/Auth/pass.js";
+import Request from "./pages/Auth/Request";
+import Card from "./pages/employee/card";
+import Pass from "./pages/Auth/pass";
+import Edit from "./pages/Admins/edit.jsx";
 
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/home" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/request" element={<Request />} />
+    <Route path="/card" element={<Card />} />
+    <Route path="/pass" element={<Pass />} />
+    <Route path="/admin" element={<Admin />} />
+    <Route path="/edit" element={<Edit />} />
+  </Routes>
+);
 
-
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/home",
-    element: <Home />,
-    errorElement: <h2>sorry....</h2>,
-  },
-
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/Request",
-    element: <Request />,
-  },
-  {
-    path: "/card",
-    element: <Card />,
-  },
-  {
-    path: "/pass",
-    element: <Pass />,
-  },
-
-  {
-    path: "/admin",
-    element: <Admin />,
-  },
-
- 
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <Router>
+      <AppRoutes />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
