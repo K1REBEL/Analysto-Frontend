47c16c5b6be233abf4c3dd3cba3e88d8cd44c2fe
ALAA-20001 <151511514+ALAA-20001@users.noreply.github.com>
2024-03-14 14:51:16 +0200

    alaa2

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import Home from "./pages/home/Home";
 import Admin from "./pages/Admins/Admin.jsx";
 import Login from "./pages/Auth/Login";
-import Request from "./pages/Org/Request";
-import Card from "./pages/User/card.js";
-import Pass from "./pages/Auth/pass.js";
+import Request from "./pages/Auth/Request";
+import Card from "./pages/employee/card.jsx";
+import Pass from "./pages/Auth/pass.jsx";
 
 
 
