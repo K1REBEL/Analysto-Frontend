014202a697ccd2a050feee399dca10db6c5d22ab
Gamalsobhy585 <eng.gamalsobhi@gmail.com>
2024-03-17 14:35:05 +0200

    react routing

--- src/App.js
+++ src/App.js
@@ -7,31 +7,6 @@ import Request from "./pages/Auth/Request";
 import Pass from "./pages/Auth/pass";
 import Card from "./pages/employee/card";
 import Edit from "./pages/Admins/Edit";
-<<<<<<< HEAD
-import Landing from "./pages/Home/Landing";
-
-import "./pages/User/card.css";
-
-function App() {
-  return (
-   
-    <>
-      <Router>
-        <Routes>
-          <Route path="/DataLink" element={<DataLink />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/request" element={<Request />} />
-          <Route path="/pass" element={<Pass />} />
-          <Route path="/card" element={<Card />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/notfound" element={<NotFound />} />
-          <Route path="/Landing" element={<Landing />} />
-        </Routes>
-      </Router>
-    </>
-  );
-=======
 import Layout from "./components/layout/layout";
 import NotFound from "./components/NotFound/NotFound";
 import {jwtDecode} from 'jwt-decode';
@@ -60,7 +35,6 @@ function App() {
     ]}
   ])
   return <RouterProvider router={routers}/>
->>>>>>> aa0949349a31ff4b55219efec081e23c2cafcc7c
 }
 
 export default App;
