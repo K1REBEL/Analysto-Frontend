a36549d6e954eb718ab05fb3e235a85cd8148b36
ALAA-20001 <151511514+ALAA-20001@users.noreply.github.com>
2024-03-20 14:35:55 +0200

    alala

--- src/pages/Org/DatatableOrg.jsx
+++ src/pages/Org/DatatableOrg.jsx
@@ -1,32 +1,62 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./DatatableOrg.scss";
-import axios from "axios";
+import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
-function DatatableOrg() {
-  const [orgEmpl, setOrgEmpl] = useState([]);
-  const [userToken, setUserToken] = useState(""); 
+import { data } from "autoprefixer";
 
-  useEffect(() => {
-    async function fetchToken() {
-      const fetchedToken = localStorage.getItem("userToken"); 
-      setUserToken(fetchedToken);
-      console.log(fetchedToken);
-      try {
-        const config = {
-          headers: {
-            Authorization: `Bearer ${fetchedToken}`
-          }
-        };
-        const { data } = await axios.get(`http://127.0.0.1:4000/api/org/empIndex`, config);
-        setOrgEmpl(data.result);
-        console.log(data.result);
-      } catch (error) {
-        console.error("Error fetching users:", error);
-      }
+function DatatableOrg() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emp, setEmp] = useState([]);
+
+// const [user, setUser] = useState({
+//   id: "",
+//   name: "",
+//   email: "",
+// });
+
+
+const getData = async () => {
+  try {
+    const response = await axios.get("http://127.0.0.1:4000/api/org/1/empIndex", {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+      },
+    });
+    const data = response.data;
+    console.log(data);
+    if (data.message === "Organization's Employees Retrieved") {
+      setEmp(data.employees);
     }
-    
-    fetchToken();
-  }, []); 
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  console.log(data)
+};
+  
+
+const handleDelete = async (id) => {
+  try {
+    const updatedEmp = emp.filter((emp) => emp.id !== id);
+    console.log("Delete user with id:", id);
+    setEmp(updatedEmp);
+    localStorage.setItem("emp", JSON.stringify(updatedEmp));
+
+    await axios.delete(`http://127.0.0.1:4000/api/org/1/emp/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+      },
+    });
+
+    getData(); // Refresh the data after deleting
+    console.log(updatedEmp);
+  } catch (error) {
+    console.error("Error deleting user:", error);
+  }
+};
 
   return (
     <div>
@@ -37,6 +67,8 @@ function DatatableOrg() {
               type="text"
               className="form-control"
               placeholder="Enter Full Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="col">
@@ -44,6 +76,8 @@ function DatatableOrg() {
               type="email"
               className="form-control"
               placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="col">
@@ -51,6 +85,8 @@ function DatatableOrg() {
               type="password"
               className="form-control"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
@@ -71,24 +107,55 @@ function DatatableOrg() {
           </tr>
         </thead>
         <tbody>
-          {orgEmpl.map((item, index) => (
-            <tr key={index}>
-              <td>
-                <input type="number" className="form-control" value={item.id} />
-              </td>
-              <td>
-                <input type="text" className="form-control" value={item.name} />
-              </td>
-              <td>
-                <input type="email" className="form-control" value={item.email} />
-              </td>
-              <td>
-                <button type="button" className="btn btn-danger btn-sm">
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
+          {emp.map((user) => {
+            return (
+              <tr>
+                <td>
+                  <input
+                    type="number"
+                    className="form-control"
+                    value={user.id}
+                    
+                  />
+
+                  <div className="d-flex align-items-center">
+                    <div className="ms-3">
+                      <p className="fw-bold mb-1">{user.id}</p>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={user.name}
+                
+                  />
+
+                  <p className="fw-normal mb-1">{user.name}</p>
+                </td>
+                <td>
+                  <input
+                    type="email"
+                    className="form-control"
+                    value={user.email}
+                   
+                  />
+
+                  <p className="fw-normal mb-1">{user.email}</p>
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    className="btn btn-danger btn-sm"
+                    onClick={() => handleDelete(user.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
--- src/pages/employee/brandsItem.jsx
+++ src/pages/employee/brandsItem.jsx
@@ -1,29 +1,92 @@
 import React from "react";
-import img6 from "../../img/logo-lg.svg";
+import amazon from "../../img/amazon.png";
+import none from "../../img/none.png";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "../../components/Navbar/Navbar";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 
 import "../employee/brandItem.scss";
 
 const BrandsItem = () => {
   return (
     <div>
+      <Navbar />
       <div className="container-fluid">
         <div className="row">
           <div className="col-12 mt-3">
             <div className="card">
+              <center>
+                <h2>Item SKUs</h2>
+              </center>
+
+              <hr />
+
               <div className="card-horizontal">
                 <div className="img-square-wrapper">
-                  <img className="" src={img6} alt="Card cap" style={{width:"200px"}}/>
+                  <div class="con p-3 ">
+                    <img className="img" src={amazon} alt="amazon" />
+                  </div>
                 </div>
-                <div className="card-body">
-                  <h4 className="card-title">Card title</h4>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <p>
-                    Example from: https://codepen.io/SteveJRobertson/pen/POdvgz
-                  </p>
+                <div class="col px-md-5" style={{ marginBottom: "20px" }}>
+                  <div class="p-3 ">
+                    <p className="text-muted smallestt">
+                      https://www.amazon.com/
+                      s?i=fashion-womens&bbn=16334314011&rh=
+                      n%3A7141123011%2Cn%3A16334314011%2Cn%3A7147
+                      440011%2Cn%3A1040660%2Cn%3A1045024%2Cp_89%3ADail
+                    </p>
+                  </div>
+                </div>
+                <div class="col px-md-5" style={{ marginBottom: "20px" }}>
+                  <button
+                    type="button"
+                    class="btn btn-primary"
+                    data-mdb-ripple-init
+                  >
+                    <AddOutlinedIcon />
+                    Add URL
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12 mt-3">
+            <div className="card">
+              <center>
+                <h2>Item SKUs</h2>
+              </center>
+
+              <hr />
+
+              <div className="card-horizontal">
+                <div className="img-square-wrapper">
+                  <div class="con p-3 ">
+                    <img className="img" src={none} alt="amazon" />
+                  </div>
+                </div>
+                <div class="col px-md-5">
+                  <div class="p-3 ">
+                    <p className="text-muted smallestt">
+                      https://www.amazon.com/
+                      s?i=fashion-womens&bbn=16334314011&rh=
+                      n%3A7141123011%2Cn%3A16334314011%2Cn%3A7147
+                      440011%2Cn%3A1040660%2Cn%3A1045024%2Cp_89%3ADail
+                    </p>
+                  </div>
+                </div>
+                <div class="col px-md-5">
+                  <button
+                    type="button"
+                    class="btn btn-primary"
+                    data-mdb-ripple-init
+                  >
+                    <AddOutlinedIcon />
+                    Add URL
+                  </button>
                 </div>
               </div>
             </div>
