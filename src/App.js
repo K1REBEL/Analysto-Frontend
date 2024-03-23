0dae422a7573087076c436bf1596a214c17bb389
ALAA-20001 <151511514+ALAA-20001@users.noreply.github.com>
2024-03-23 14:13:32 +0200

    brand

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Admin from "./pages/Admins/Admin";
 import Login from "./pages/Auth/Login";
 import Request from "./pages/Auth/Request";
 import Pass from "./pages/Auth/pass";
-import Card from "./pages/employee/card";
+import Card from "./components/employee/card";
 import Edit from "./pages/Admins/Edit";
 import Layout from "./components/layout/layout";
 import NotFound from "./components/NotFound/NotFound";
 import {jwtDecode} from 'jwt-decode';
 import Landing from "./pages/home/Landing";
 import RequestAdmin from "./pages/request-admin/RequestAdmin";
-import BrandsItem from "./pages/employee/brandsItem";
+import BrandsItem from "./components/employee/brandsItem";
 function App() {
 
   const [userData , setuserData ] = useState(null);
